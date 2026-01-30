c13e9a043df29ec4ee81d0326fd170929519b27c
Arjun <arjunjayesh500@gmail.com>
2026-01-31 03:08:38 +0530

    Fix 500 Error: Add missing django.contrib.sitemaps to INSTALLED_APPS

--- dearco_portfolio/settings.py
+++ dearco_portfolio/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'cloudinary',
     'tinymce',
     
